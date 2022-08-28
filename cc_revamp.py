844419b96c352f92c9f8e39cc60aa36ce9b57dff
GUNGET1 <nikitenkonikita2004@gmail.com>
2022-08-28 23:52:47 +0300

    Так-так, что тут у нас?

--- cc_revamp.py
+++ cc_revamp.py
@@ -2,13 +2,18 @@ import json
 import requests
 import xml.etree.ElementTree as ET
 from fastapi import FastAPI
+import uvicorn
 
 valutes_dict = {}
+name_to_CharCode = {}
+
 app = FastAPI()
 
 class CB_Valute_Extract():  
     def init(self):
         global valutes_dict
+        global name_to_CharCode
+
         url = "http://www.cbr.ru/scripts/XML_daily.asp?"
         response = requests.get(url)
         string = response.content
@@ -23,12 +28,14 @@ class CB_Valute_Extract():
         for valute in parsed_response.findall('Valute'):
 
             valutes_dict[valute.find('Name').text] = valute.find('Value').text.replace(",", ".")
+            name_to_CharCode[valute.find('CharCode').text] = valute.find('Name').text
     
             print(valute.find('Name').text)
             print(valute.find('Value').text)
+            print(valute.find('CharCode').text)
 
     def getSalaryRub(self, amount, valute):
-        result = float(valutes_dict[valute]) * amount
+        result = float(valutes_dict[name_to_CharCode.get(valute)]) * amount
 
         json_result = json.dumps(str(result))
         print(json.loads(json_result))
@@ -41,10 +48,14 @@ try:
     example.init()
 
     @app.get("/getCurrency/")
-    def read_item(curr: str = None, amount: float = 0):
+    async def read_item(curr: str = 'USD', amount: float = 0):
         print(curr)
         print(amount)
         return example.getSalaryRub(amount, curr)
+
+#    if __name__ == "__main__":
+#        uvicorn.run(app, host="0.0.0.0", port=8000)
+# Это для дебагера.
 
 
 except AttributeError:
